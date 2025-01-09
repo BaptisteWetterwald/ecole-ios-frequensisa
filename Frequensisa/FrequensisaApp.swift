e8c53aebb7e9648e0bef1764766c1947373c909a
Baptiste Wetterwald <baptiste.wetterwald@gmail.com>
2025-01-09 10:56:31 +0100

    Init with project files

---
+++ Frequensisa/FrequensisaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrequensisaApp.swift
+//  Frequensisa
+//
+//  Created by ENSISA on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FrequensisaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FrequensisaTests/FrequensisaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FrequensisaTests.swift
+//  FrequensisaTests
+//
+//  Created by ENSISA on 09/01/2025.
+//
+
+import Testing
+@testable import Frequensisa
+
+struct FrequensisaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
